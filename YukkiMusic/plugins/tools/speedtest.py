c3922b10cabaea384db15e0b6025d9313495f6b5
sukunbots <110039556+sukunbots@users.noreply.github.com>
2022-08-20 20:49:28 +0530

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -39,18 +39,18 @@ async def speedtest_function(client, message):
     m = await message.reply_text("Running Speed test")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**Speedtest Results**
+    output = f"""**ᴍᴜsɪᴄ sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs**
     
-<u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+<u>**ᴍᴜsɪᴄ sɪʟᴇɴᴛ:**</u>
+**__ɪsᴘ:__** {result['client']['isp']}
+**__ᴄᴏᴜɴᴛʀʏ:__** {result['client']['country']}
   
-<u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
-**__Ping:__** {result['ping']}"""
+<u>**ᴍᴜsɪᴄ sᴇʀᴠᴇʀ:**</u>
+**__ɴᴀᴍᴇ:__** {result['server']['name']}
+**__ᴄᴏᴜɴᴛʀʏ:__** {result['server']['country']}, {result['server']['cc']}
+**__sᴘᴏɴsᴇʀ:__** {result['server']['sponsor']}
+**__ʟᴀᴛᴇɴᴄʏ:__** {result['server']['latency']}  
+**__ᴘɪɴɢ:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, 
         photo=result["share"], 
