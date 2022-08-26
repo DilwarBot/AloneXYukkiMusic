0607850d748b21dd813dd716bb55185b3bcff87d
Team Sukun <110039556+sukunbots@users.noreply.github.com>
2022-08-26 13:45:41 +0530

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -121,7 +121,7 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 🛃**<u>HEROKU:</u>**
 /usage - Dyno Usage.
 
-🌐**<u>CONFIG VARS:</u>**
+😚**<u>CONFIG VARS:</u>**
 /get_var - Get a config var from Heroku or .env.
 /del_var - Delete any var on Heroku or .env.
 /set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
@@ -140,17 +140,17 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 /activevideo - Check active video calls on bot.
 /stats - Check Bots Stats
 
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
+🥶**<u>BLACKLIST CHAT FUNCTION:</u>**
 /blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
 /whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
 /blacklistedchat - Check all blacklisted chats.
 
-👤**<u>BLOCKED FUNCTION:</u>**
+🥵**<u>BLOCKED FUNCTION:</u>**
 /block [Username or Reply to a user] - Prevents a user from using bot commands.
 /unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
 /blockedusers - Check blocked Users Lists
 
-👤**<u>GBAN FUNCTION:</u>**
+🔥**<u>GBAN FUNCTION:</u>**
 /gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
 /ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
 /gbannedusers - Check Gbanned Users Lists
@@ -159,12 +159,12 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 /set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
 /videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
 
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
+🙀**<u>PRIVATE BOT FUNCTION:</u>**
 /authorize [CHAT_ID] - Allow a chat for using your bot.
 /unauthorize [CHAT_ID] - Disallow a chat from using your bot.
 /authorized - Check all allowed chats of your bot.
 
-🌐**<u>BROADCAST FUNCTION:</u>**
+🎊**<u>BROADCAST FUNCTION:</u>**
 /broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
 
 <u>options for broadcast:</u>
@@ -174,6 +174,6 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 **-assistant** : This will broadcast your message from assistant account of your bot.
 **-nobot** : This will force your bot to not broadcast message
 
-**Example:** `/broadcast -user -assistant -pin Hello Testing`
+**Example:** `/broadcast -user -assistant -pin Hello Sukun`
 
 """
